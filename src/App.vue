8c9c925f9b43aa30a344bc7617e49d852e05f43c
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-25 20:42:06 +0100

    startup vuejs project with tailwind

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-full text-blue-500">
+    hellos
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppCompo'
+});
+
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+    './public/**/*.html',
+    './src/**/*.vue'
+  ],
+  theme: {
+    extends: {},
+  },
+  variants: {},
+  plugins: [],
+};  
